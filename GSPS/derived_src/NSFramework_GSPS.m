38ff71309f753165332c745adb7158f0fac695ed
Gregory John Casamento <greg.casamento@gmail.com>
2025-06-15 03:49:41 -0400

    Add initial files

---
+++ GSPS/derived_src/NSFramework_GSPS.m
@@ -0,0 +1,10 @@
+#include <Foundation/NSString.h>
+@interface NSFramework_GSPS : NSObject
++ (NSString *)frameworkVersion;
++ (NSString *const*)frameworkClasses;
+@end
+@implementation NSFramework_GSPS
++ (NSString *)frameworkVersion { return @"0"; }
+static NSString *allClasses[] = {NULL};
++ (NSString *const*)frameworkClasses { return allClasses; }
+@end
